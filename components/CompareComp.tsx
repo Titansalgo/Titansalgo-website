0cdb41bf32f70b4baf97a29809f9749609cd3295
Titansalgo <anshaal.forex@gmail.com>
2025-02-06 04:36:26 +0530

    desing

--- components/CompareComp.tsx
+++ components/CompareComp.tsx
@@ -14,7 +14,7 @@ const CompareComp = () => {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="relative w-[68.125rem] h-[68.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-50"
+          className="relative w-[68.125rem] h-[68.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#d900ff] to-[#2a628d] opacity-70"
         />
       </div>
 
@@ -28,7 +28,7 @@ const CompareComp = () => {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="relative w-[68.125rem] h-[68.125rem] -translate-x-1/2 -translate-y-1/2 rotate-[120deg] bg-gradient-to-tr from-[#00ffff] to-[#7f00ff] opacity-50"
+          className="relative w-[68.125rem] h-[68.125rem] -translate-x-1/2 -translate-y-1/2 rotate-[120deg] bg-gradient-to-tr from-[#0062ff] to-[#aa00ff] opacity-50"
         />
       </div> */}
 
@@ -42,15 +42,15 @@ const CompareComp = () => {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
           }}
-          className="relative w-[68.125rem] h-[68.125rem] translate-x-1/3 rotate-[210deg] bg-gradient-to-tr from-[#0ff] to-[#ff1493] opacity-50"
+          className="relative w-[68.125rem] h-[68.125rem] translate-x-1/3 rotate-[210deg] bg-gradient-to-tr from-[#48249c] to-[#0b43a3] opacity-70"
         />
       </div>
 
       {/* Compare component */}
       <div className="p-4 rounded-3xl">
         <Compare
-          firstImage="https://cdn.prod.website-files.com/62f11b068296f36584c1ec32/6606172379d796e7b5845114_rdsfasd34.png"
-          secondImage="https://cdn.prod.website-files.com/62f11b068296f36584c1ec32/660617245e1b01c5af3b20de_gdsasdfg342.png"
+          firstImage="https://message.style/cdn/images/6ff940fae6913bd5dbd663309501bfb2602d4a60a79e7a129a7172426543749e.png"
+          secondImage="https://message.style/cdn/images/24ebcfb8e66c4920147b6ec7f6a1ffb6ba667fc1d47783ab08223623c05e869d.png"
           firstImageClassName="object-fill object-left-top"
           secondImageClassname="object-fill object-left-top"
           className="h-[250px] w-[400px] sm:h-[350px] sm:w-[600px] md:h-[500px] md:w-[900px]"
